8ff72eb3170055b20845ca43f1a2418c15300e8e
Logan Johnson <logan.johnson189@gmail.com>
2022-12-22 03:35:38 +0000

    Add logo to page

--- ryoko-ui/src/components/Header.js
+++ ryoko-ui/src/components/Header.js
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types'
 import { useLocation } from 'react-router-dom'
 import Button from './Button'
+import Icon from '../assets/logo.jpg'
 
 const Header = ({ title, onAdd, showAdd }) => {
   const location = useLocation()
 
   return (
     <header className='header'>
-      <h1>{title}</h1>
+      <div className='header'>
+        <img src={Icon} alt='Logo' className='logo' />
+        <h1>{title}</h1>
+      </div>
       {location.pathname === '/' && (
         <Button
           color={showAdd ? '#ED6A5A' : '#F0BE19'}
